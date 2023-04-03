5ce9de4f7cbccee4670265d3ed76f9974b449af7
itsbittoosharma <utkarshup562@gmail.com>
2023-04-03 16:06:40 +0530

    Added option to create new page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponseNotFound
+from django import forms
 from django.urls import reverse
 from . import util
 import markdown2
+
+class NewPageForm(forms.Form):
+    title = forms.CharField(label="Title ", max_length=20,min_length=3,required=True)
+    content = forms.CharField(widget=forms.Textarea())
 
 def index(request):
     if request.GET.__contains__('q'):
@@ -32,3 +37,24 @@ def wiki(request, title):
         "title":title,
         "content":content
                   })
+    
+def newpage(request):
+    if request.method=="GET":
+        return render(request,"encyclopedia/newpage.html",
+                    {
+            "title":"Create New Page",
+            "form":NewPageForm()
+                    })  
+    else:
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            title=form.cleaned_data['title']
+            content=form.cleaned_data['content']
+            util.save_entry(title,content)
+            return redirect(reverse("wiki",kwargs={"title":title}))
+        else:
+            return render(request,"encyclopedia/newpage.html",
+                          {
+                "title":"Create New Page",
+                "form":NewPageForm(form)
+                          })
